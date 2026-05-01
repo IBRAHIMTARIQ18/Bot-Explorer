2515e967a508ec70c96ae7b7a13dfd4fd453bd25
IBRAHIMTARIQ18 <ibrahimtariq2014@gmail.com>
2026-05-01 08:02:14 +0500

    Refactor App component and update styles in App.css for consistency

--- src/App.js
+++ src/App.js
@@ -2,38 +2,43 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
-import './App.css';
-import Scroll from './Scroll';
+import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: robots,
-            searchfield: ''
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        })
-     return(
-        <div className="tc">
-            <h1 className="f1"> ROBOSEARCH </h1>
-            <SearchBox searchChange = {this.onSearchChange}/>
-            <Scroll>
-                <CardList robots={filteredRobots} />
-            </Scroll>
-            <h1 className="f3"> BUILT BY M.IBRAHIM TARIQ </h1>
-
-        </div>
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1"> BOT EXPLORER </h1>
+        <h2 className="second-heading">Discover AI Robots</h2>
+        <p className="description-para">
+          Search through a futuristic directory of intelligent digital entities.
+        </p>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+        <h1 className="f3"> BUILT BY M.IBRAHIM TARIQ </h1>
+      </div>
     );
+  }
 }
-};
 
-export default App
+export default App;
